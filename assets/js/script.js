82e69edf2c351ab5daa759a1883330407cfb6e84
BrandonKutivan <bskutivan@gmail.com>
2020-04-05 14:24:11 -0700

    laid out criteria

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// when i click the start button - then a timer starts and I am presented with a question
+
+// when I answer a question then I am presented with another question
+
+// when I answer a question incorrectly then time is subtracted from the clock
+
+//when all questions are answered or the timer reaches 0 then the game is over
+
+//when the game is over then I can save my initials and score
