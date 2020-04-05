f6f29014ae3c1773a8a9fc84a769ea85a712b5bb
BrandonKutivan <bskutivan@gmail.com>
2020-04-05 14:42:37 -0700

    laid out pseudocode based on criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
 // when i click the start button - then a timer starts and I am presented with a question
 
-// when I answer a question then I am presented with another question
+// create  timer variable that starts at 0
+// start quiz function that adds 75 to the clock and calls a countdown function
+// call a function that adds hide-me class to start screen
+// call a function to cycle through question array with 
 
+// when I answer a question then I am presented with another question
 // when I answer a question incorrectly then time is subtracted from the clock
+
+// call a function that checks answer for correct or incorrect
 
 //when all questions are answered or the timer reaches 0 then the game is over
 
+// call a function that removes hide-me class for end screen
+
 //when the game is over then I can save my initials and score
+
+// when user submits initials call function that collects this information and stores it and takes us to the highscore page.
+// call function that gets this data from local storage and displays it on the final page.
